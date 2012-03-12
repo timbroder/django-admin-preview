2f480342548c752c44bc8807a91bd1d45426e359
Phil Christensen <pchristensen@freelancersunion.org>
2012-03-12 10:59:07 -0400

    added missing imports, comment cleanup

--- adminpreview/admin.py
+++ adminpreview/admin.py
@@ -1,7 +1,13 @@
+from django.contrib import admin
+from django.conf.urls.defaults import patterns, url
+
 class PreviewAdmin(admin.ModelAdmin):
-    #add to your ModelAdmin
-    #list_display = ('headline','created_date', 'state', 'admin_slide_preview')
     def admin_slide_preview(self, obj):
+        """
+        Add this to your ModelAdmin:
+        
+        list_display = ('headline','created_date', 'state', 'admin_slide_preview')
+        """
         return "<div class=\"previewslide\" id=\"%s/preview/\">+</div>" % obj.id
     admin_slide_preview.allow_tags = True
     admin_slide_preview.short_description = 'Preview'
@@ -13,7 +19,7 @@ class PreviewAdmin(admin.ModelAdmin):
 
     def get_urls(self):
         my_urls = patterns('',
-            (r'^(?P<object_id>\d+)/preview/$', self.admin_site.admin_view(self.get_preview)),
+            url(r'^(?P<object_id>\d+)/preview/$', self.admin_site.admin_view(self.get_preview)),
         )
         return my_urls + super(PreviewAdmin, self).get_urls()
 
